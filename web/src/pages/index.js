9a995ca77c668957d0d5447d9c446541446e57fd
Rishabh Varshney <it.is.rishabh@gmail.com>
2020-10-13 07:13:34 -0400

    remove log

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -77,8 +77,6 @@ const IndexPage = props => {
   const site = (data || {}).site
   const page = (data || {}).page
 
-  console.log(page)
-
   if (!site) {
     throw new Error(
       'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
